e587ae23f4c4dec887586809f13a82da7ea7c52b
Joseph Cashin <18jcashin@holyrood.bep.ac>
2023-09-14 11:47:14 +0100

    starter for saving plan

---
+++ starters.py
@@ -0,0 +1,11 @@
+#14/09/23
+total = 0
+final = 0
+paid = int(input("paid in"))
+years = int(input("years"))
+for i in range(years):
+    start = final
+    total = final + paid
+    interest = total * 0.1
+    final = total + interest
+    print(start,paid,final,interest,i, "\n")
