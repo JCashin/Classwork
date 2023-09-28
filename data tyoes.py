1a54da5f2942963328e4e518a0d8836ec57e8fd1
Joseph Cashin <18jcashin@holyrood.bep.ac>
2023-09-28 11:49:17 +0100

    temperature code

---
+++ data tyoes.py
@@ -0,0 +1,7 @@
+#string = str(input("what is your name"))
+#integer = int(input("how old are you"))
+#
+#floating = float(input("what is height in metres"))
+#print(string, integer, floating)
+print(18.234 // 9)
+print(10.2312 % 4)
---
+++ for loop lesson.py
@@ -0,0 +1,10 @@
+#z=7
+#y=z*12
+#for i in range(0,y,z):
+#    print(i+z)
+import random
+a = random.randint(1,10)
+x = 0
+while x != a:
+   x = int(input("guess a number 1-10"))
+print("you win")
